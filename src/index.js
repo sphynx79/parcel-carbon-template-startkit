e08235122f990d89557b57166a7b288f7f1701c1
sphynx79 <miboscol@gmail.com>
2019-04-29 20:20:04 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./index.scss"
+
+// import { Dropdown } from "carbon-components";
+
+// Dropdown.create(document.getElementById("my-dropdown"));
+import { HeaderNav } from "carbon-components";
+import { HeaderSubmenu } from "carbon-components";
+import { SideNav } from "carbon-components";
+import { NavigationMenu } from "carbon-components";
+import { NavigationMenuPanel } from "carbon-components";
+import { ProductSwitcher } from "carbon-components";
+import { OverflowMenu } from "carbon-components";
+import { Search } from "carbon-components";
+
+// HeaderNav.create(document.getElementById("data-header-nav"));
+HeaderNav.init();
+HeaderSubmenu.init();
+SideNav.init();
+NavigationMenu.init();
+// NavigationMenuPanel.init();
+ProductSwitcher.init();
+OverflowMenu.init();
+Search.init();
